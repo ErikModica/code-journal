59bbb01bf2e8125bc69188fa8f47c6ac9b3c23f9
Erik Modica <erikmodica@gmail.com>
2021-02-08 15:56:52 -0800

    cleaned up code and made sure everything is spick and span. no errors run into

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ $imgURL.addEventListener('input', function (event) {
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  entryFormInfo[$entryForm.elements.imageUrl.name] = $entryForm.elements.imageUrl.value;
   entryFormInfo[$entryForm.elements.title.name] = $entryForm.elements.title.value;
+  entryFormInfo[$entryForm.elements.imageUrl.name] = $entryForm.elements.imageUrl.value;
   entryFormInfo[$entryForm.elements.notes.name] = $entryForm.elements.notes.value;
   entryFormInfo.nextEntryId = data.nextEntryId;
   data.nextEntryId += 1;
@@ -23,8 +23,8 @@ $entryForm.addEventListener('submit', function (event) {
 
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 
-  $entryForm.elements.imageUrl.value = null;
   $entryForm.elements.title.value = null;
+  $entryForm.elements.imageUrl.value = null;
   $entryForm.elements.notes.value = null;
 });
 
