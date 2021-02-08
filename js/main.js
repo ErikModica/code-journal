03d6a0cbbb16efc7b666661111fedc53e046d660
Erik Modica <erikmodica@gmail.com>
2021-02-08 13:30:45 -0800

    added a input listner to the image-url input to change the image when a link is pasted

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $imgURL = document.querySelector('input');
+var $image = document.querySelector('.entry-image');
+$imgURL.addEventListener('input', function (event) {
+
+  $image.setAttribute('src', event.target.value);
+});
