c367ce9e206837e2e0b4451e6aef9db2eb158a17
Erik Modica <erikmodica@gmail.com>
2021-02-08 15:23:05 -0800

    added submit event listener to the form and implemented a system that adds the values to an object that is further stored into an array inside the data object.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 /* global data */
 /* exported data */
 
-var $imgURL = document.querySelector('input');
+var $imgURL = document.querySelector('.img-url-input');
 var $image = document.querySelector('.entry-image');
-$imgURL.addEventListener('input', function (event) {
+var $entryForm = document.querySelector('.entry-form');
+var entryFormInfo = {};
 
+$imgURL.addEventListener('input', function (event) {
   $image.setAttribute('src', event.target.value);
+});
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  entryFormInfo[$entryForm.elements.imageUrl.name] = $entryForm.elements.imageUrl.value;
+  entryFormInfo[$entryForm.elements.title.name] = $entryForm.elements.title.value;
+  entryFormInfo[$entryForm.elements.notes.name] = $entryForm.elements.notes.value;
+  entryFormInfo.nextEntryId = data.nextEntryId;
+  data.nextEntryId += 1;
+
+  data.entries.unshift(entryFormInfo);
+
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  $entryForm.elements.imageUrl.value = null;
+  $entryForm.elements.title.value = null;
+  $entryForm.elements.notes.value = null;
 });
